b564e64d046c99948c8629fee2ba7db4fa8797af
Ubuntu <ubuntu@ip-172-31-43-208.us-east-2.compute.internal>
2025-01-25 19:46:44 +0000

    some_bug

--- jaxrl/agents/drq/weight_recyclers.py
+++ jaxrl/agents/drq/weight_recyclers.py
@@ -81,12 +81,12 @@ def weight_revive(param, next_param, dead_neuron_mask, key,
       dead_neuron_mask, param, next_param
   )
   key, subkey = random.split(key)
-  noise = jax.random.normal(subkey) * jnp.abs(param) * eps
+  noise = jax.random.normal(subkey, shape=param.shape) * jnp.abs(param) * eps
   param = jnp.where(
     dead_incoming_mask, new_incoming_param + noise, param
   )
   key, subkey = random.split(key)
-  noise = jax.random.normal(subkey) * jnp.abs(param) * eps
+  noise = jax.random.normal(subkey, shape=param.shape) * jnp.abs(param) * eps
   next_param = jnp.where(
     dead_outgoing_mask, new_outgoing_param + noise, next_param
   )
