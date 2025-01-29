bef09de271d8b5d3e64e9de79e6c5a1bc4853f9b
Ubuntu <ubuntu@ip-172-31-4-148.us-east-2.compute.internal>
2025-01-29 16:25:23 +0000

    logging_2w

--- examples/configs/drq_weight_pruning.py
+++ examples/configs/drq_weight_pruning.py
@@ -25,7 +25,7 @@ def get_config():
 
     config.batch_size_statistics = 256
     config.dead_neurons_thresholds = [0.0, 0.025, 0.1]
-    config.dormancy_logging_period = 2000
+    config.dormancy_logging_period = 20000
 
     config.discount = 0.99
 
