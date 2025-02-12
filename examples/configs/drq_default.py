c18a28f3cd32a8461a099d1d8890602967dfe9d0
ZifanWu <wuzf5@mail2.sysu.edu.cn>
2025-02-12 02:46:49 +0000

    default_logging_period

--- examples/configs/drq_default.py
+++ examples/configs/drq_default.py
@@ -46,7 +46,7 @@ def get_config():
 
     config.batch_size_statistics = 256
     config.dead_neurons_thresholds = [0.0, 0.1]
-    config.dormancy_logging_period = 2000
+    config.dormancy_logging_period = 5000
 
     config.discount = 0.99
 
