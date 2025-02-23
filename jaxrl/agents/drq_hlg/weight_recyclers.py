5367f8c1a806c66306736dd6eed6b1f7301868ab
ZifanWu <wuzf5@mail2.sysu.edu.cn>
2025-02-23 19:26:13 +0000

    preactivation_minus_b

--- jaxrl/agents/drq_hlg/weight_recyclers.py
+++ jaxrl/agents/drq_hlg/weight_recyclers.py
@@ -318,14 +318,14 @@ class BaseRecycler:
     return False
 
   def is_intermediated_required(self, update_step):
-    return self.is_logging_step(update_step) # TODO debugging
+    return True#self.is_logging_step(update_step) # TODO debugging
 
   def is_logging_step(self, step):
     return step % self.dormancy_logging_period == 0
 
   def maybe_log_deadneurons(self, update_step, intermediates, preactivations, params):
     is_logging = self.is_logging_step(update_step)
-    if is_logging: # TODO debugging
+    if True:#is_logging: # TODO debugging
       self.log_historical_dead_neuron_overlapping(intermediates, preactivations, params, update_step)
   
   def _compute_mask(self, score_dict):
@@ -398,7 +398,10 @@ class BaseRecycler:
         if self.track and 'dense' in k and ('critic0' in k or 'actor' in k or 'layernorm' in k):
           wandb.log({'{}_mean_activation'.format(layer_name): jnp.mean(activation).tolist(), 'grad_step': update_step})
           wandb.log({'{}_mean_preactivation'.format(layer_name): jnp.mean(preactivation).tolist(), 'grad_step': update_step})
-          # wandb.log({'{}_std_preactivation'.format(layer_name): jnp.std(preactivation), 'grad_step': update_step})
+          if 'dense1' in k and ('critic0' in k):
+            bias_key = 'CriticHead/' + layer_name + '/bias'
+            bias = param_dict[bias_key]
+            wandb.log({'{}_preactivation_minus_b'.format(layer_name): jnp.mean(jnp.mean(preactivation, axis=0)  - bias).tolist(), 'grad_step': update_step})
 
           # we want to check if the preactivation distribution within a layer is some Gaussian, 
           # so we check if quantiles matches the theoretical quantiles of the Gaussian with that mean and that std
